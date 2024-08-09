062879a6daaaabc7519363b0d794a0487bd43dd9
Elio Tohme <eliotohme@gmail.com>
2024-08-09 18:55:00 +0200

    [chore]add comments

--- install-odoo.sh
+++ install-odoo.sh
@@ -4,4 +4,5 @@
 print_headline "installing odoo"
 helm upgrade --install odoo oci://registry-1.docker.io/bitnamicharts/odoo -f odoo.values.yaml --wait
 
+print_headline "setup odoo vs"
 kubectl apply -f odoo-vs.yaml
